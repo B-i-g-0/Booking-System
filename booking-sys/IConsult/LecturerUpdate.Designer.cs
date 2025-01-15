71f03ac2b4d8e1ff1473ec659bdcdb5c4ced5613
B-i-g-0 <145435070+B-i-g-0@users.noreply.github.com>
2025-01-15 16:54:51 +0200

    New files added to repository.

---
+++ booking-sys/IConsult/LecturerUpdate.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace IConsult
+{
+    partial class LecturerUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxSchool = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtLectNo = new System.Windows.Forms.TextBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtSchool = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtOffice = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(41, 229);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Office";
+            // 
+            // comboBoxSchool
+            // 
+            this.comboBoxSchool.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.comboBoxSchool.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSchool.ForeColor = System.Drawing.Color.AliceBlue;
+            this.comboBoxSchool.FormattingEnabled = true;
+            this.comboBoxSchool.Items.AddRange(new object[] {
+            "School Of Business Sciences",
+            "School Of Accountancy",
+            "School Of Applied Mathematics And Computer Sciences"});
+            this.comboBoxSchool.Location = new System.Drawing.Point(175, 187);
+            this.comboBoxSchool.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxSchool.Name = "comboBoxSchool";
+            this.comboBoxSchool.Size = new System.Drawing.Size(132, 24);
+            this.comboBoxSchool.TabIndex = 13;
+            this.comboBoxSchool.SelectedIndexChanged += new System.EventHandler(this.comboBoxSchool_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 257);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Course";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 162);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 16);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "School";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 124);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 90);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "First Name";
+            // 
+            // txtLName
+            // 
+            this.txtLName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtLName.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtLName.Location = new System.Drawing.Point(175, 116);
+            this.txtLName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(132, 22);
+            this.txtLName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 55);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Lecturer Number";
+            // 
+            // txtFName
+            // 
+            this.txtFName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtFName.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtFName.Location = new System.Drawing.Point(175, 81);
+            this.txtFName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(132, 22);
+            this.txtFName.TabIndex = 5;
+            // 
+            // txtLectNo
+            // 
+            this.txtLectNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtLectNo.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtLectNo.Location = new System.Drawing.Point(175, 52);
+            this.txtLectNo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtLectNo.Name = "txtLectNo";
+            this.txtLectNo.ReadOnly = true;
+            this.txtLectNo.Size = new System.Drawing.Size(132, 22);
+            this.txtLectNo.TabIndex = 4;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.btnSignUp.ForeColor = System.Drawing.Color.AliceBlue;
+            this.btnSignUp.Location = new System.Drawing.Point(23, 361);
+            this.btnSignUp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(132, 42);
+            this.btnSignUp.TabIndex = 3;
+            this.btnSignUp.Text = "UPDATE";
+            this.btnSignUp.UseVisualStyleBackColor = false;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.AliceBlue;
+            this.panel2.Controls.Add(this.txtCourse);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.txtSchool);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.txtOffice);
+            this.panel2.Controls.Add(this.comboBoxSchool);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtLName);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txtFName);
+            this.panel2.Controls.Add(this.txtLectNo);
+            this.panel2.Controls.Add(this.btnSignUp);
+            this.panel2.Location = new System.Drawing.Point(371, 73);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(325, 410);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtCourse.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtCourse.Location = new System.Drawing.Point(173, 254);
+            this.txtCourse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(132, 22);
+            this.txtCourse.TabIndex = 19;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "INFO2001A",
+            "INFO2000A",
+            "COMS2014A",
+            "COMS2015A",
+            "ACCN2014A",
+            "ACCN2013A"});
+            this.comboBox2.Location = new System.Drawing.Point(175, 284);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(132, 24);
+            this.comboBox2.TabIndex = 18;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // txtSchool
+            // 
+            this.txtSchool.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtSchool.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtSchool.Location = new System.Drawing.Point(175, 154);
+            this.txtSchool.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtSchool.Name = "txtSchool";
+            this.txtSchool.Size = new System.Drawing.Size(132, 22);
+            this.txtSchool.TabIndex = 17;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.button3.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button3.Location = new System.Drawing.Point(175, 361);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 42);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtOffice
+            // 
+            this.txtOffice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.txtOffice.ForeColor = System.Drawing.Color.AliceBlue;
+            this.txtOffice.Location = new System.Drawing.Point(175, 220);
+            this.txtOffice.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtOffice.Name = "txtOffice";
+            this.txtOffice.Size = new System.Drawing.Size(132, 22);
+            this.txtOffice.TabIndex = 15;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 32);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1068, 524);
+            this.panel1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.button2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button2.Location = new System.Drawing.Point(912, -1);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "BACK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.button1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(992, -1);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(125)))), ((int)(((byte)(166)))));
+            this.label10.Location = new System.Drawing.Point(8, 7);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 16);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "I-CONSULT";
+            // 
+            // LecturerUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LecturerUpdate";
+            this.Text = "LecturerUpdate";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxSchool;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtLectNo;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtSchool;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtOffice;
+    }
+}
